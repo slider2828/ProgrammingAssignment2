da89f386f55ac2265038f6b391967ec6d3deb039
slider2828 <kenyu@hotmail.com>
2016-03-04 13:53:09 -0800

    Updated R File

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,58 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inversematrix <- NULL
+  set <- function(y) {
+    x <<- y
+    inversematrix <<- NULL
+  }
+  get <- function() x
+  setinversematrix <- function(inverse) inversematrix <<- inverse
+  getinversematrix <- function() inversematrix
+  list(set = set, get = get,
+       setinversematrix = setinversematrix,
+       getinversematrix = getinversematrix)
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inversematrix <- x$getinversematrix()
+  
+  if(!is.null(inversematrix)) {
+    message("getting cached data")
+    return(inversematrix)
+  }
+  
+  mat.data <- x$get()
+  inversematrix <- solve(mata.data, ...)
+  
+  x$setinversematrix(inversematrix)
+  
+  return(inversematrix)
+}
+
+
+x <- c(1, 2)
+y <- c(2,1)
+z <- rbind(x, y)
+
+
+solve(z) %*% z
+
+test = function(mat){
+  ## @mat: an invertible matrix
+  
+  temp = makeCacheMatrix(mat)
+  
+  start.time = Sys.time()
+  cacheSolve(temp)
+  dur = Sys.time() - start.time
+  print(dur)
+  
+  start.time = Sys.time()
+  cacheSolve(temp)
+  dur = Sys.time() - start.time
+  print(dur)
 }
